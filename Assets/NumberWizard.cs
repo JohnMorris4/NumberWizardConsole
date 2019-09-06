5e559b8c88c96a0aa3590ca04137cc8ff9fad9bb
johnmorris4 <johnmorris4@bellsouth.net>
2019-09-06 11:17:18 -0400

    Finished section.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+    //Variables placed at class level
+    int maxGuess;
+    int minGuess;
+    int guess;
     // Start is called before the first frame update
     void Start()
     {
-        var maxGuess = 1000;
-        var minGuess = 1;
-       Debug.Log("Welcome to Number Wizard");
-
-       Debug.Log("Pick A Number, don't tell me what it is");
-
-
-       Debug.Log("Highest Number that you can pick is " + maxGuess);
-
-       Debug.Log("Lowest Number that you can pick is " + minGuess);  
-       Debug.Log("Tell me if your number is higher or lower than my guess");
-       Debug.Log("Push UP = Higher, Push DOWN = Lower, Press ENTER for correct");
+       StartGame();
+    }
+    void StartGame() 
+    {
+        maxGuess = 1000;
+        minGuess = 1;
+        guess = 500;
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick A Number, don't tell me what it is");
+        Debug.Log("Highest Number that you can pick is " + maxGuess);
+        Debug.Log("Lowest Number that you can pick is " + minGuess);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push UP = Higher, Push DOWN = Lower, Press ENTER for correct");
+        maxGuess = maxGuess + 1;
     }
 
     // Update is called once per frame
@@ -27,15 +33,24 @@ public class NumberWizard : MonoBehaviour
         //Setup the arrow keys to check to see if they were pressed
         if(Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Debug.Log("You Pressed the Down Arrow");
+            minGuess = guess;
+            NextGuess();
         }
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+         else if(Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("You pressed the Up Arrow");
+            maxGuess = guess;
+            NextGuess();
         }
-        if(Input.GetKeyDown(KeyCode.Return))
+        else if(Input.GetKeyDown(KeyCode.Return))
          {
             Debug.Log("You pressed the enter key");
+            StartGame();
          }
+    }
+
+    void NextGuess()
+    {
+        guess = (maxGuess + minGuess) / 2;
+        Debug.Log("Is it higher or lower than " + guess);
     }
 }
